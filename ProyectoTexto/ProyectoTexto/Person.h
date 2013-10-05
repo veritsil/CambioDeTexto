44be681fc20bbb59fd0ed73075ecebe3a104f635
veritsil <silvia.cachi.t@gmail.com>
2013-10-04 23:25:44 -0400

    creacion de la clase Person

---
+++ ProyectoTexto/ProyectoTexto/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  ProyectoTexto
+//
+//  Created by Silvia Cachi Tenorio on 10/4/13.
+//  Copyright (c) 2013 Silvia Cachi Tenorio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
